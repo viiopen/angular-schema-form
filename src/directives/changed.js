ba2fbfa1895e1b70c646ef0cb8e6d1c456a2e2e3
Shade L. Jenifer <shade.l.jenifer@gmail.com>
2016-03-08 08:49:41 -0500

    Checkboxes can be cleared based on a clearValue

--- src/directives/changed.js
+++ src/directives/changed.js
@@ -5,7 +5,7 @@
  * Takes the form definition as argument.
  * If the form definition has a "onChange" defined as either a function or
  */
-angular.module('schemaForm').directive('sfChanged', function() {
+angular.module('schemaForm').directive('sfChanged', ['$timeout', function($timeout) {
   return {
     require: 'ngModel',
     restrict: 'AC',
@@ -14,15 +14,34 @@ angular.module('schemaForm').directive('sfChanged', function() {
       var form = scope.$eval(attrs.sfChanged);
       //"form" is really guaranteed to be here since the decorator directive
       //waits for it. But best be sure.
-      if (form && form.onChange) {
-        ctrl.$viewChangeListeners.push(function() {
-          if (angular.isFunction(form.onChange)) {
-            form.onChange(ctrl.$modelValue, form);
-          } else {
-            scope.evalExpr(form.onChange, {'modelValue': ctrl.$modelValue, form: form});
-          }
-        });
+      if (form) {
+        var clearValue = form.clearValue || (form.schema && form.schema.clearValue ? form.schema.clearValue : undefined);
+
+        if (form.onChange) {
+          ctrl.$viewChangeListeners.push(function() {
+            if (angular.isFunction(form.onChange)) {
+              form.onChange(ctrl.$modelValue, form);
+            } else {
+              scope.evalExpr(form.onChange, {'modelValue': ctrl.$modelValue, form: form});
+            }
+          });
+        } else if (angular.isDefined(clearValue)) {  // because clearValue can = 0
+          ctrl.$viewChangeListeners.push(function() {
+            $timeout(function() {
+              var ptr = scope.model;
+
+              for (k in form.key) {
+                ptr = ptr[form.key[k]]
+              }
+
+              if (ptr.indexOf(0) > -1) {
+                var key = "['" + form.key.join("']['") + "']";
+                eval("scope.model" + key + " = [0]");
+              }
+            });
+          });
+        }
       }
     }
   };
-});
+}]);
