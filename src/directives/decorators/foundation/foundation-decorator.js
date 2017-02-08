7ae202ac1851af1015a7c03f627db6b005677738
Paul Salaets <psalaets@viimed.com>
2017-02-08 13:00:38 -0500

    viinetwork/i#4204 define a new time type which is just hours, minutes, am/pm -- no date

--- src/directives/decorators/foundation/foundation-decorator.js
+++ src/directives/decorators/foundation/foundation-decorator.js
@@ -45,6 +45,7 @@ angular.module('schemaForm').config(['schemaFormDecoratorsProvider', function(de
     'tabarray': {template: base + 'tabarray.html', replace: false},
     'tabs': {template: base + 'tabs.html', replace: false},
     'textarea': {template: base + 'textarea.html', replace: false},
+    'time': {template: base + 'time.html', replace: false},
     'validator': {template: base + 'validator.html', replace: false},
     'weight': {template: base + 'weight.html', replace: false}
     /*
@@ -98,6 +99,7 @@ angular.module('schemaForm').config(['schemaFormDecoratorsProvider', function(de
     'submit': base + 'submit.html',
     'text': base + 'default.html',
     'textarea': base + 'textarea.html',
+    'time': base + 'time.html',
     'validator': {template: base + 'validator.html', replace: false},
     'weight': {template: base + 'weight.html', replace: false}
     /*
