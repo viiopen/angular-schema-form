36e215cd6f375c5571be2b4d4d189e03b6697a82
Patrick Hall-Wurst <phallwurst@viimed.com>
2017-05-17 17:14:54 -0400

    new validator

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -1862,6 +1862,100 @@ if (!customValidators) {
 
 if (!customValidators) {
 
+  console.log("Cannot define validator validateCnsaComplications()", new Date());
+
+} else {
+
+  customValidators.validateCnsaCervicalComplications = function(viewValue, form, model) {
+    var fieldId = form.fieldId;
+    var element_ids = [];
+    var noMsg = {};
+    var id;
+
+    // make sure the model is current
+    model[form.key[0]] = viewValue
+
+    var reasons = [
+      "toggle_complication_reason_dvt",
+      "toggle_complication_reason_pe",
+      "toggle_complication_reason_mi",
+      "toggle_complication_reason_uti",
+      "toggle_complication_reason_neuro",
+      "toggle_complication_reason_ssi",
+      "toggle_complication_reason_hematoma",
+      "toggle_complication_reason_cva",
+      "toggle_complication_reason_pneum",
+      "toggle_complication_reason_seroma",
+      "toggle_complication_reason_instFail",
+      "toggle_complication_reason_dysphagia_tube",
+      "toggle_complication_reason_dysphagia_no_tube",
+      "toggle_complication_reason_vocal_cord_paralysis",
+      "toggle_complication_reason_cfs_leak",
+      "toggle_complication_reason_other"
+    ];
+
+    var noneSelected = true;
+
+    for (var i=0; i < reasons.length; i++) {
+      /* uncomment this to highlight all the checkboxes if none are selected */
+
+      id = 'field-' + reasons[i] + '-' + fieldId;
+      element_ids.push(id);
+
+      // this element doesn't need a specific error message, so...
+      noMsg[id] = true;
+
+      // check if reason exists
+      noneSelected = noneSelected && !model[reasons[i]];
+    }
+
+    if (noneSelected) {
+      // in the form, the help decorator looks like a field label, so...
+      element_ids.push('help-reasons-for-complication-' + fieldId);
+
+      // in order for the error message to appear in the validator element...
+      element_ids.push('val-reasons-for-complication-' + fieldId);
+
+      return {
+        custom: true,
+        valid: false,
+        error: { code: 0, element_ids: element_ids, noMsg: noMsg },
+        rootScopeBroadCast: true
+      }
+
+    } else if (model['toggle_complication_reason_other'] && _isEmpty(model['complication_reason_other_specify'])) {
+
+      // basically the same as the above block, sans comments
+      element_ids = [];
+      id = 'field-complication_reason_other_specify-' + fieldId;
+      element_ids.push(id);
+      noMsg[id] = true;
+      element_ids.push('help-reasons-for-complication-' + fieldId);
+      element_ids.push('val-reasons-for-complication-' + fieldId);
+
+      return {
+        custom: true,
+        valid: false,
+        error: { code: 0, element_ids: element_ids, noMsg: noMsg },
+        rootScopeBroadCast: true
+      }
+
+    } else {
+
+      // send the help decorator's ID back so schema-validate
+      // can remove the error class
+
+      element_ids.push('help-reasons-for-complication-' + fieldId);
+      return { valid:true, clear: element_ids }
+
+    }
+
+  }
+
+}
+
+if (!customValidators) {
+
 } else {
     customValidators.validateCnsaCervicalDiagnosis = function(viewValue, form, model) {
 
@@ -2030,10 +2124,11 @@ if (!customValidators) {
           };
         } else if (model.toggle_clinical_manifestation) {
             if (!(
-              model.toggle_cm_radiculopathy ||
-              model.toggle_cm_myelopathy ||
-              model.toggle_cm_neuroBowelBladd ||
-              model.toggle_cm_lowestMotor
+                model.toggle_cm_radiculopathy ||
+                model.toggle_cm_myelopathy ||
+                model.toggle_mechanicalInstability ||
+                model.toggle_cm_neuroBowelBladd ||
+                model.toggle_cm_lowestMotor
             )) {
                 return {
                     custom: true,
@@ -2153,8 +2248,11 @@ if (!customValidators) {
             if (model.nd_open === null) {
                 element_ids.push('field-nd_open-' + fieldId);
             }
-            if (model.nd_laminoplasty === null) {
-                element_ids.push('field-nd_laminoplasty-' + fieldId);
+            if (model.nd_procedure === null) {
+                element_ids.push('field-nd_procedure-' + fieldId);
+            }
+            if (model.nd_procedure === 3 && model.nd_procedure_foraminotomy_side === null) {
+                element_ids.push('field-nd_procedure_foraminotomy_side-' + fieldId);
             }
 
             var levels = [
@@ -3426,6 +3524,8 @@ angular.module('schemaForm').service('customValidators', [
     functions.validateCnsaDiagnosis = customValidators.validateCnsaDiagnosis;
 
     functions.validateCnsaTreatment = customValidators.validateCnsaTreatment;
+
+    functions.validateCnsaCervicalComplications = customValidators.validateCnsaCervicalComplications;
 
     functions.validateCnsaComplications = customValidators.validateCnsaComplications;
 
