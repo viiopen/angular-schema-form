231c017227db2fd6446275951a4148f9b4a9ccda
Patrick Hall-Wurst <phallwurst@viimed.com>
2018-08-27 14:33:40 -0400

    forgot gulp

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -1867,7 +1867,7 @@ if (!customValidators) {
     var fieldId = form.fieldId;
     var element_ids;
 
-    if (model.employment === 1) {
+    if (model.employment == 1) {
       var employedFields = [
         'full_part_time',
         'occupation_level',
@@ -1897,7 +1897,7 @@ if (!customValidators) {
       }
     }
 
-    if (model.employment === 2) {
+    if (model.employment == 2) {
       var notWorkingFields = [
         'employed_not_working',
         'occupation_level',
@@ -1927,59 +1927,67 @@ if (!customValidators) {
       }
     }
 
-    if (model.employment === 3 && !model.unemployed) {
-      return {
-        custom: true,
-        valid: false,
-        error: {
-          code: 0,
-          element_ids: [
-            'field-unemployed-' + fieldId
-          ]
-        },
-        rootScopeBroadCast: true
+    if (model.employment == 3) {
+      if (!model.unemployed) {
+        return {
+          custom: true,
+          valid: false,
+          error: {
+            code: 0,
+            element_ids: [
+              'field-unemployed-' + fieldId
+            ]
+          },
+          rootScopeBroadCast: true
+        };
       }
     }
 
-    if (model.act_out_home === 1 && !model.describe_act_out) {
-      return {
-        custom: true,
-        valid: false,
-        error: {
-          code: 0,
-          element_ids: [
-            'field-describe_act_out-' + fieldId
-          ]
-        },
-        rootScopeBroadCast: true
+    if (model.act_out_home == 1) {
+      if (!model.describe_act_out) {
+        return {
+          custom: true,
+          valid: false,
+          error: {
+            code: 0,
+            element_ids: [
+              'field-describe_act_out-' + fieldId
+            ]
+          },
+          rootScopeBroadCast: true
+        };
       }
     }
 
-    if (model.act_in_home === 1 && !model.describe_act_in) {
-      return {
-        custom: true,
-        valid: false,
-        error: {
-          code: 0,
-          element_ids: [
-            'field-describe_act_in-' + fieldId
-          ]
-        },
-        rootScopeBroadCast: true
+    if (model.act_in_home == 1) {
+      if (!model.describe_act_in) {
+        return {
+          custom: true,
+          valid: false,
+          error: {
+            code: 0,
+            element_ids: [
+              'field-describe_act_in-' + fieldId
+            ]
+          },
+          rootScopeBroadCast: true
+        };
       }
     }
 
-    if ((model.smoking_status === 1 || model.smoking_status === 2) && !model.smoking_cessation) {
-      return {
-        custom: true,
-        valid: false,
-        error: {
-          code: 0,
-          element_ids: [
-            'field-smoking_cessation-' + fieldId
-          ]
-        },
-        rootScopeBroadCast: true
+    if (model.smoking_status == 1 || model.smoking_status == 2) {
+      if (!model.smoking_cessation) {
+        return {
+          custom: true,
+          valid: false,
+          error: {
+            code: 0,
+            element_ids: [
+              'field-smoking_cessation-' + fieldId
+            ]
+          },
+          rootScopeBroadCast: true
+        };
       }
     }
 
